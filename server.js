bc46a70cf59a0def13dfd115a2b434575a26e1a4
chrishall71 <chrishallsrq@gmail.com>
2020-01-03 19:11:28 -0600

    added index.html file

--- server.js
+++ server.js
@@ -24,10 +24,6 @@ http.createServer((request, response) => {
 			console.log("Added to log.");
 		}
 		
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write (data);
-	response.end('Hello Node!\n');
-		
 	})
 
 }).listen(8080);
