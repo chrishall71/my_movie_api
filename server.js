70f4fcb0711d35b699002ce537afea4917f65207
chrishall71 <chrishallsrq@gmail.com>
2020-01-05 10:58:08 -0600

    Rewrote server.js code
    I had to make a few adjustments to the server.js code but found out that my computer was locking down 8080 port.. had to kill and then the achievement worked

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 const http = require('http'),
-	url = require('url'),
-	fs = require('fs');
+   fs = require('fs'),
+   url = require('url');
 
-	//this creates new server
-http.createServer((request, response) => {
-	var addr = request.url,
-		q = url.parse (addr, true),
-		filePath = '';
-
-		//checks for the documentation file
-	if (q.pathname.includes('documentation')) {
-		filePath = (__dirname + '/documentation.html');
-	} else {
-		filePath = 'index.html';
-	}
-
-	fs.appendFile('log.txt', 'url: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("Added to log.");
-		}
-		
-		
-	response.writeHead(200, { 'Content-Type': 'text/html' });
-	response.write(data);
-	response.end();
-
-	});
-
+   http.createServer((request, response) => {
+   var addr = request.url,
+      q = url.parse(addr, true),
+      filePath = '';
+   
+	  if (q.pathname.includes('documentation')) {
+      filePath = (__dirname + '/documentation.html');
+   } else {
+      filePath = 'index.html';
+   }
+   
+   fs.readFile(filePath, function(err, data) {
+      if (err) {
+         throw err;
+      }
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(data);
+      response.end();
+   });
 }).listen(8080);
