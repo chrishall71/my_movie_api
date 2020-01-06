049a3555487626d3d90bf8a7ed9d43d077d920cb
chrishall71 <chrishallsrq@gmail.com>
2020-01-05 22:11:05 -0600

    Put code back for lesson 2.4
    works properly

--- index.js
+++ index.js
@@ -1,28 +1,66 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+	morgan = require('morgan'),
+	app = express();
 
-var myLogger = (req, res, next) => {
-	console.log(req.url);
-	next();
-};
+var myMovies = [
+	{
+		title: 'Toy Story 4',
+		year: '2019'
+	},
+	{
+		title: 'Coco',
+		year: '2017'	
+	},
+	{
+		title: 'Up',
+		year: '2009'	
+	},
+	{
+		title: 'Finding Nemo',
+		year: '2003'	
+	},
+	{
+		title: 'The Lego Movie',
+		year: '2014'	
+	},
+	{
+		title: 'Mary Poppins',
+		year: '1964'	
+	},
+	{
+		title: '101 Dalmation',
+		year: '1961'
+	},
+	{
+		title: 'Ghostbuters',
+		year: '1984'
+	},
+	{
+		title: 'Back to the Future',
+		year: '1985'
+	},
+	{
+		title: 'The Truman Show',
+		year: '1998'
+	},
+];
 
-var requestTime = (req, res, next)=>{
-	req.requestTime = Date.now();
-	next();
-}
+app.use(morgan ('common'));//log all request with Morgan
+app.use(express.static('public')); //retrieves files from public folder
 
-app.use(myLogger);
-app.use(requestTime);
-app.get('/', function (req, res){
-	var responseText = 'Welcome to my app!';
-	responseText += '<small> Requested at: ' + req.requestTime + '</small>';
-	res.send(responseText);
+//GET Request
+app.get('/movies', function (req, res) {
+	res.json(myMovies);
+  });
+
+app.get('/', function (req, res) {
+	res.send('My Movie Home Page');
 });
 
-app.get('/secreturl', (req, res)=>{
-	var responseText = 'This is a secret site!';
-	responseText += '<small> Requested at: ' + req.requestTime + '</small>';
-	res.send(responseText);
+//Error Handling in Express
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
 });
 
-app.listen(8080);
+app.listen(8080); 
