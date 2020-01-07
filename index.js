e38f088cb44e6f1043556ce01d3c406f016cfec4
chrishall71 <chrishallsrq@gmail.com>
2020-01-07 11:20:56 -0600

    lesson 2.5 first commit
    Added GET, POST, DELET for movies, users, directors

--- index.js
+++ index.js
@@ -1,61 +1,303 @@
+//IMPORT DEPENDENCIES
 const express = require('express'),
 	morgan = require('morgan'),
-	app = express();
+	uuid = require ('uuid'),
+	bodyParser = require('body-parser');
+	
+const app = express();	
 
-var myMovies = [
+	app.use(morgan ('common'));//log all request with Morgan
+	app.use(express.static('public')); //retrieves files from public folder
+	app.use(bodyParser.json());
+
+//USER Data base
+let Users = [
 	{
+		id: 1,
+		Name:"John Doe",
+		username:"jonhdoe81",
+		password:'Password1234',
+		birthday:'october 19, 1971',
+		email:'john@gmail.com'
+	}
+];
+
+
+let Genre = [
+	{
+		name:'Drama',
+		discription:"Drama is a genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone"
+	},
+	{
+		name:'Action',
+		discription:"Action film is a film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, and frantic chases. Action films tend to feature a resourceful hero struggling against incredible odds, which include life-threatening situations, a villain, or a pursuit which usually concludes in victory for the hero.",
+	},
+	{
+		name:'Sci-Fi',
+		discription:'Science fiction (often abbreviated Sci-Fi or SF) is a genre of speculative fiction that has been called the "literature of ideas". It typically deals with imaginative and futuristic concepts such as advanced science and technology, time travel, parallel universes, fictional worlds, space exploration, and extraterrestrial life. Science fiction often explores the potential consequences of scientific innovations.'
+	},
+	{
+		name:'Comedy',
+		discription:"A comedy film is a genre of film in which the main emphasis is on humor."
+	},
+	{
+		name:'animation',
+		discription:"Animation is a method in which pictures are manipulated to appear as moving images. ",
+	}
+];
+
+
+let Directors = [
+	{
+		name:'Josh Cooley',
+		bio:"Joshua Cooley (born May 23, 1980) is an American animator, screenwriter, director and voice actor. He is best known for working on the 2015 Pixar animated film Inside Out and directing the short film Riley's First Date? that was included in its home video release. He made his feature film directorial debut in 2019 with Toy Story 4, the fourth installment of the Toy Story film franchise series.",
+		birthyear:'1980',
+		deathyear:''
+	},
+	{
+		name:'Ron Howard',
+		bio:"Academy Award-winning filmmaker Ron Howard is one of this generation's most popular directors. From the critically acclaimed dramas A Beautiful Mind (2001) and Apollo 13 (1995) to the hit comedies Parenthood (1989) and Splash (1984), he has created some of Hollywood's most memorable films.",
+		birthyear:'1954',
+		deathyear:''
+	},
+	{
+		name:'Andrew Stanton',
+		bio:"Oscar-winning filmmaker Andrew Stanton was raised in Rockport, Massachusetts. He was educated at The California Institute of the Arts in Los Angeles, where he studied character animation. After graduation, Stanton began working as a writer on the TV series Mighty Mouse: The New Adventures. In 1990, he became only the second animator and ninth employee to join Pixar Animation Studios. Stanton went on to help establish Pixar as one of the world's leading animation studios",
+		birthyear:'1965',
+		deathyear:''
+	},{
+		name:'Stephen Spielberg',
+		bio:"One of the most influential personalities in the history of cinema, Steven Spielberg is Hollywood's best known director and one of the wealthiest filmmakers in the world. He has an extraordinary number of commercially successful and critically acclaimed credits to his name, either as a director, producer or writer since launching the summer blockbuster with Jaws (1975), and he has done more to define popular film-making since the mid-1970s than anyone else.",
+		birthyear:'1946',
+		deathyear:''
+	},{
+		name:'Frank Darabont',
+		bio:"Three-time Oscar nominee Frank Darabont was born in a refugee camp in 1959 in Montbeliard, France, the son of Hungarian parents who had fled Budapest during the failed 1956 Hungarian revolution. Brought to America as an infant, he settled with his family in Los Angeles and attended Hollywood High School. His first job in movies was as a production assistant on the 1981 low-budget film, Hell Night (1981), starring Linda Blair. He spent the next six years working in the art department as a set dresser and in set construction while struggling to establish himself as a writer. His first produced writing credit (shared) was on the 1987 film, A Nightmare on Elm Street 3: Dream Warriors (1987), directed by Chuck Russell. Darabont is one of only six filmmakers in history with the unique distinction of having his first two feature films receive nominations for the Best Picture Academy Award: 1994's The Shawshank Redemption (1994) (with a total of seven nominations) and 1999's The Green Mile (1999) (four nominations). ",
+		birthyear:'1959',
+		deathyear:''
+	},
+	{
+		name:'Justin Lin',
+		bio:"Justin Lin is a Taiwanese-American film director whose films have grossed $2 billion worldwide. He is best known for his work on Better Luck Tomorrow, The Fast and the Furious 3-6 and Star Trek Beyond. He is also known for his work on television shows like Community and the second season of True Detective. Lin was born in Taipei, Taiwan, and grew up in a working-class neighborhood in Cypress, California, in Orange County. He attended Cypress High School and University of California, San Diego for two years before transferring to UCLA, where he earned a B.A. in Film & Television and a MFA in Film Directing & Production from the UCLA film school.",
+		birthyear:'1973',
+		deathyear:''
+	},{
+		name:'David Leitch',
+		bio:"David Leitch is a billion dollar film director, actor, stuntman, writer, producer, and stunt coordinator. He co-directed John Wick (2014) with Chad Stahelski, which he also served as producer. David directed Atomic Blonde (2017) starring Charlize Theron. David also directed the box office smash and critically acclaimed Deadpool 2 (2018). He is the director of Fast and Furious spin off: Hobbs and Shaw (2019).",
+		birthyear:'1975',
+		deathyear:''
+	},
+	{
+		name:'Todd Phillips',
+		bio:"Todd Phillips was born on December 20, 1970 in Brooklyn, New York City, New York, USA as Todd Bunzl. He is a producer and director, known for Joker (2019), Old School (2003) and Due Date (2010).",
+		birthyear:'1970',
+		deathyear:''
+	}
+];
+
+let Movies = [
+	{
+		id: 1,
 		title: 'Toy Story 4',
-		year: '2019'
+		year: '2019',
+		description: 'Toy Story 4 is a 2019 American computer-animated comedy film produced by Pixar Animation Studios for Walt Disney Pictures. It is the fourth installment in Pixars Toy Story series.',
+		genre: 'animation',
+		director: 'Josh Cooley',
+		imageUrl: 'https://upload.wikimedia.org/wikipedia/en/4/4c/Toy_Story_4_poster.jpg',
+		featured: 'false'
 	},
 	{
-		title: 'Coco',
-		year: '2017'	
+		id: 2,
+		title: 'A Beautiful Mind',
+		year: '2001',
+		description: 'A Beautiful Mind: a Biography of John Forbes Nash, Jr., Winner of the Nobel Prize in Economics, 1994',
+		genre: 'drama',
+		director: 'Ron Howard',
+		imageUrl: 'https://upload.wikimedia.org/wikipedia/en/b/b8/A_Beautiful_Mind_Poster.jpg',
+		featured: 'false'	
 	},
 	{
-		title: 'Up',
-		year: '2009'	
+		id: 3,
+		title: 'In the Heart of the Sea',
+		year: '2009',
+		description: "A recounting of a New England whaling ship's sinking by a giant whale in 1820, an experience that later inspired the great novel Moby-Dick.",
+		genre: 'drama',
+		director: 'Ron Howard',
+		imageUrl: 'https://upload.wikimedia.org/wikipedia/en/8/8b/In_the_Heart_of_the_Sea_poster.jpg',
+		featured: 'false'	
 	},
 	{
+		id: 4,
 		title: 'Finding Nemo',
-		year: '2003'	
+		year: '2003',
+		description: 'After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.',
+		genre: 'animation',
+		director: 'Andrew Stanton',
+		imageUrl: 'https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg',
+		featured: 'false'	
 	},
 	{
-		title: 'The Lego Movie',
-		year: '2014'	
+		id: 5,
+		title: 'Catch Me If You Can',
+		year: '2002',
+		description: "A seasoned FBI agent pursues Frank Abagnale Jr. who, before his 19th birthday, successfully forged millions of dollars' worth of checks while posing as a Pan Am pilot, a doctor, and a legal prosecutor.",
+		genre: 'drama',
+		director: 'Stephen Spielberg',
+		imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTY5MzYzNjc5NV5BMl5BanBnXkFtZTYwNTUyNTc2._V1_UX182_CR0,0,182,268_AL_.jpg',
+		featured: 'false'	
 	},
 	{
-		title: 'Mary Poppins',
-		year: '1964'	
+		id: 6,
+		title: 'The Shawshank Redemption',
+		year: '1994',
+		description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+		genre: 'drama',
+		director: 'Frank Darabont',
+		imageUrl: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
+		featured: 'false'	
 	},
 	{
-		title: '101 Dalmation',
-		year: '1961'
+		id: 7,
+		title: 'Fast and Furious',
+		year: '2009',
+		description: "Brian O'Conner, back working for the FBI in Los Angeles, teams up with Dominic Toretto to bring down a heroin importer by infiltrating his operation.",
+		genre: 'action',
+		director: 'Justin Lin',
+		imageUrl: 'https://m.media-amazon.com/images/M/MV5BYjQ1ZTMxNzgtZDcxOC00NWY5LTk3ZjAtYzRhMDhlNDZlOWEzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+		featured: 'false'
+	},
+	{	id: 8,
+		title: 'Star Trek Beyond',
+		year: '2016',
+		description: "The crew of the USS Enterprise explores the furthest reaches of uncharted space, where they encounter a new ruthless enemy, who puts them, and everything the Federation stands for, to the test.",
+		genre: 'Sci-Fi',
+		director: 'Justin Lin',
+		imageUrl: 'https://m.media-amazon.com/images/M/MV5BZDRiOGE5ZTctOWIxOS00MWQwLThlMDYtNWIwMDQwNzBjZDY1XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
+		featured: 'false'
 	},
 	{
-		title: 'Ghostbuters',
-		year: '1984'
+		id: 9,
+		title: 'Hobbs & Shaw',
+		year: '2019',
+		description: "Lawman Luke Hobbs (Dwayne Johnson) and outcast Deckard Shaw (Jason Statham) form an unlikely alliance when a cyber-genetically enhanced villain threatens the future of humanity.",
+		genre: 'action',
+		director: 'David Leitch',
+		imageUrl: 'https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SY1000_CR0,0,685,1000_AL_.jpg',
+		featured: 'false'
 	},
 	{
-		title: 'Back to the Future',
-		year: '1985'
-	},
-	{
-		title: 'The Truman Show',
-		year: '1998'
+		id: 10,
+		title: 'The Hangover',
+		year: '2009',
+		description: "Three buddies wake up from a bachelor party in Las Vegas, with no memory of the previous night and the bachelor missing. They make their way around the city in order to find their friend before his wedding.",
+		genre: 'comedy',
+		director: 'Todd Phillips',
+		imageUrl: 'https://m.media-amazon.com/images/M/MV5BNGQwZjg5YmYtY2VkNC00NzliLTljYTctNzI5NmU3MjE2ODQzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+		featured: 'false'
 	},
 ];
 
-app.use(morgan ('common'));//log all request with Morgan
-app.use(express.static('public')); //retrieves files from public folder
-
 //GET Request
+app.get('/', function (req, res) {
+	res.redirect('/index.html');
+});
+
+//--MOVIES--
+
+// Get list of data about ALL Movies (GET)
 app.get('/movies', function (req, res) {
-	res.json(myMovies);
+	res.json(Movies);
   });
 
-app.get('/', function (req, res) {
-	res.send('My Movie Home Page');
+  //Get one movie by title
+app.get('/movies/:title', (req, res) =>{
+	res.json(Movies.find( (movie) => {
+		return movie.title === req.params.title
+	}));
 });
+
+
+//Add new movie to the list (POST)
+app.post('/add_movie', (req, res) => {
+	let newMovie = req.body;
+
+	if (!newMovie.title) {
+		const message = 'Missing name in request body';res.status(400).send(message);
+	} else {
+		newMovie.id = uuid.v4();//create unique ID
+		Movies.push(newMovie);
+		res.status(201).send(newMovie);
+	}
+});
+
+// Delete a movie from the list
+app.delete('/movies/:id', (req, res) => {
+	let movie = Movies.find((movie) => {
+		return movie.id === req.params.id
+	});
+
+	if (movie) {
+		Movie.filter (function(obj) {
+			return obj.id !== req.params.id
+		});
+		res.status(201).send('Movie' + movie.title + 'with id' + req.params.id + 'was deleted.')
+	}
+});
+
+// --DIRECTORS--
+
+// Get list of all directors
+app.get('/directors', (req, res) => {
+	res.json(Directors);
+});
+
+//Get one director by name
+app.get('/directors/:name', (req, res) => {
+	res.json(Directors.find( (director) => {
+		return director.name === req.params.name
+	}));
+});
+
+//--GENRE--
+
+//Get a list of ALL the genres
+app.get('/genres', (req, res) => {
+	res.json(Genre);
+});
+
+//Get one movie Genre
+app.get('/genres/:name', (req, res) => {
+	res.json(Genre.find( (genre) => {
+		return genre.name === req.params.name
+	}));
+});
+
+//--USERS--
+
+//Adds data for a new user to the list of USERS.
+app.post('/users', (req, res) => {
+	let newUser = req.body;
+
+	if(!newUser.name) {
+		const message = 'Missing name in request body';
+		res.status(400).send(message);
+	} else {
+		newUser.id = uuid.v4();//create unique ID
+		Users.push(newUser);
+		res.status(201).send(newUser);
+	}
+});
+
+// Deletes a user from the list by ID
+app.delete('/users/:id', (req, res) => {
+	let user = Users.find((user) => { return user.id === req.params.id; });
+  
+	if (user) {
+	  Users = Users.filter(function(obj) { return obj.id !== req.params.id; });
+	  res.status(201).send('User ' + user.name + ' with id ' + req.params.id + ' was deleted.')
+	}
+  });
+  
 
 //Error Handling in Express
 app.use((err, req, res, next) => {
@@ -63,4 +305,7 @@ app.use((err, req, res, next) => {
 	res.status(500).send('Something broke!');
 });
 
-app.listen(8080); 
+//START SERVER
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
