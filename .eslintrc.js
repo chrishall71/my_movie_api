ba3ad9b402f066bd9968d05683c65b1eb24e33c7
chrishall71 <chrishallsrq@gmail.com>
2020-02-03 11:32:15 -0600

    new eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,28 @@
 module.exports = {
-	"extends": "airbnb",
-	"parser": "babel-eslint",
-	"parserOptions": {
-		"sourceType": "module",
-		"ecmaFeatures": {
-			"jsx": true,
-			"modules": true
-		}
+	"env": {
+		"browser": true,
+		"es6": true,
 	},
-	"plugins": ["prettier"],
-	"extends": ["prettier"],
+	"extends": [
+		"plugin:react/recommended",
+		"airbnb",
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
+		},
+		"ecmaVersion": 2018,
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react",
+		"@typescript-eslint"
+	],
 	"rules": {
-		"prettier/prettier": "error"
 	}
 };
