95536dc44bf7e051cabf98614b90564bc8e4e5b5
chrishall71 <chrishallsrq@gmail.com>
2020-02-09 13:13:19 -0600

    Update src.78399e21.js

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -49477,7 +49477,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56395" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65529" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
